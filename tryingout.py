447bf5c0b0bc3f3ccc627515b5fcece70ceb8fd9
sierracs123 <sierratien@gmail.com>
2025-06-15 21:55:34 -0400

    Update tryingout.py

--- tryingout.py
+++ tryingout.py
@@ -3,8 +3,6 @@ import streamlit as st
 st.set_page_config(
     page_title="Multipage App"
 )
-
-st.title("Home Page")
 st.sidebar.success("Select a Quiz")
 
 def what_harry_potter_character_are_you():
